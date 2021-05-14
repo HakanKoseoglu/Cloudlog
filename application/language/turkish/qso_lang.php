6f77ad9c3daa0aa2ff2a11b013ac90075ce34470
Hakan Koseoglu at jupiter <hakan@koseoglu.org>
2021-05-14 08:36:39 +0000

    Turkish Language WIP

---
+++ application/language/turkish/qso_lang.php
@@ -0,0 +1,30 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+// Tiles
+$lang['qso_title_qso_map'] = 'QSO Haritası';
+$lang['qso_title_suggestions'] = 'Öneriler';
+$lang['qso_title_pervious_contacts'] = 'Önceki Görüşmeler';
+
+// Input Help Text on the /QSO Display
+$lang['qso_transmit_power_helptext'] = 'Gücü Watt olarak veriniz. Sadece rakamları giriniz.';
+
+$lang['qso_sota_ref_helptext'] = 'Örnek: GM/NS-001.';
+
+$lang['qso_sig_helptext'] = 'Örnek: WWFF or POTA';
+$lang['qso_sig_info_helptext'] = 'Örnek: DLFF-0029';
+
+$lang['qso_dok_helptext'] = 'Örnek: Q03';
+
+$lang['qso_notes_helptext'] = 'Not içeriği yalnızca Cloudlog içinde kullanılır ve diğer hizmetlere aktarılmaz';
+
+// Button Text on /qso Display
+
+$lang['qso_btn_reset_qso'] = 'Baştan başla';
+$lang['qso_btn_save_qso'] = 'QSOyu kaydet';
+$lang['qso_btn_edit_qso'] = 'QSOyu düzenle';
+
+// QSO Details
+
+$lang['qso_details'] = 'QSO detayları';
