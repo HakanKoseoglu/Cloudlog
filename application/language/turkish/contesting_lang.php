e08ccda240a904ff0609b78a791b7b89529b50de
Hakan Koseoglu at jupiter <hakan@koseoglu.org>
2021-05-14 08:36:39 +0000

    Turkish Language WIP

---
+++ application/language/turkish/contesting_lang.php
@@ -0,0 +1,18 @@
+<?php
+
+defined('BASEPATH') OR exit('No direct script access allowed');
+
+$lang['contesting_page_title'] = 'Contest Logging';
+$lang['contesting_button_reset_contest_session'] = 'Yarışma oturumunu baştan başlat';
+
+$lang['contesting_exchange_type'] = 'Haberleşme Tipi';
+$lang['contesting_exchange_type_serial'] = 'Seri numarası';
+$lang['contesting_exchange_type_other'] = 'Diğer';
+
+$lang['contesting_contest_name'] = 'Yarışma İsmi';
+
+$lang['contesting_btn_reset_qso'] = 'QSOyu baştan başlat';
+$lang['contesting_btn_save_qso'] = 'QSOyu kaydet';
+
+$lang['contesting_title_callsign_suggestions'] = 'Çağrı Kodu Önerileri';
+$lang['contesting_title_contest_logbook'] = 'Yarışma kayıt kitabı';
