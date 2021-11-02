0678fc642611fde77e21eac2a49d835f3238fa9c
Hakan <hakan@koseoglu.org>
2021-11-02 22:58:33 +0000

    WIP

--- application/language/turkish/qslcard_lang.php
+++ application/language/turkish/qslcard_lang.php
@@ -7,12 +7,12 @@ $lang['qslcard_string_your_are_using'] = 'QSL kart varlıklarınızı depolamak 
 $lang['qslcard_string_disk_space'] = 'kadar disk alanı kullanıyorsunuz';
 
 $lang['qslcard_info'] = 'QSL Bilgileri';
-$lang['qslcard_sent_bureau'] = 'QSL kardı büro üzerinden gönderildi';
-$lang['qslcard_sent_direct'] = 'QSL kardı direkt gönderildi';
-$lang['qslcard_recv_bureau'] = 'QSL kardı büro üzerinden alındı';
-$lang['qslcard_recv_direct'] = 'QSL kardı direkt alındı';
+$lang['qslcard_sent_bureau'] = 'QSL kartı büro üzerinden gönderildi';
+$lang['qslcard_sent_direct'] = 'QSL kartı direkt gönderildi';
+$lang['qslcard_recv_bureau'] = 'QSL kartı büro üzerinden alındı';
+$lang['qslcard_recv_direct'] = 'QSL kartı direkt alındı';
 
-$lang['qslcard_upload_front'] = 'Yüklenen QSL kardının ön resmi';
-$lang['qslcard_upload_back'] = 'Yüklenen QSL kardının arka resmi';
+$lang['qslcard_upload_front'] = 'Yüklenen QSL kartının ön resmi';
+$lang['qslcard_upload_back'] = 'Yüklenen QSL kartının arka resmi';
 
 $lang['qslcard_upload_button'] = 'QSL kardını yükle';
